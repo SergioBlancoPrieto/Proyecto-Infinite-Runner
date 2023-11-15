3269fd2ff32dd63889796eb33a390496b64c145d
Λsh Dev <69819379+ash-dvlpr@users.noreply.github.com>
2023-11-15 23:35:13 +0100

    Revert "Test autoformat"
    This reverts commit 5d1cd4f9572a4ed75cff07b1b8f79e8767efd804.

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -2,49 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ItemType {
-    health,
-    money
+public enum ItemType
+{
+	health,
+	money
 }
 
-public class Item : MonoBehaviour {
-    public ItemType type;
-    public AudioClip itemSound;
-    private bool isCollected;
-    public int value;
+public class Item : MonoBehaviour
+{
+	public ItemType type;
+	public AudioClip itemSound;
+	private bool isCollected;
+	public int value;
 
-    void Show() {
-        GetComponent<SpriteRenderer>().enabled = true;
-        GetComponent<CircleCollider2D>().enabled = true;
-        isCollected = false;
+	void Show()
+	{
+		GetComponent<SpriteRenderer>().enabled = true;
+		GetComponent<CircleCollider2D>().enabled = true;
+		isCollected = false;
+        
+	}
 
-    }
+	void Hide()
+	{
+		GetComponent<SpriteRenderer>().enabled = false;
+		GetComponent<CircleCollider2D>().enabled = false;
+	}
 
-    void Hide() {
-        GetComponent<SpriteRenderer>().enabled = false;
-        GetComponent<CircleCollider2D>().enabled = false;
-    }
+	void Collect()
+	{
+		isCollected = true;
+		Hide();
+		//GameManager.sharedInstance.CollectItem(value);
+		AudioSource.PlayClipAtPoint(itemSound, gameObject.transform.position, 1f);
 
-    void Collect() {
-        isCollected = true;
-        Hide();
-        //GameManager.sharedInstance.CollectItem(value);
-        AudioSource.PlayClipAtPoint(itemSound, gameObject.transform.position, 1f);
-
-        switch (type) {
-            case ItemType.money: {
-                GameManager.sharedInstance.CollectItem(value);
-                break;
-            }
-            case ItemType.health: {
-                PlayerController.sharedInstance.CollectHealth(value);
-                break;
-            }
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("Player")) {
+		switch (type)
+		{
+			case ItemType.money:
+			{
+				GameManager.sharedInstance.CollectItem(value);
+				break;
+			}
+			case ItemType.health:
+			{
+				PlayerController.sharedInstance.CollectHealth(value);
+				break;
+			}
+		}
+	}
+	
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
             Collect();
         }
     }
